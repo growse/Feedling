008ed00f3b82bce4c0198e00b84b47f8581b3994
growse <growse@1b7a0db6-1847-0410-be85-accc0b0973a7>
2009-12-28 20:59:53 +0000

    For some reason, there was a missing brace in this file.
    git-svn-id: https://feedling.svn.sourceforge.net/svnroot/feedling/trunk@42 1b7a0db6-1847-0410-be85-accc0b0973a7

--- Feedling/App.xaml.cs
+++ Feedling/App.xaml.cs
@@ -6,7 +6,7 @@ namespace Feedling
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : Application {
     
         public App()
         {
