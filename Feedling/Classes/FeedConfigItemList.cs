6fe20da838e1d8f49e60c6236d85fa41377f15f1
growse <growse@1b7a0db6-1847-0410-be85-accc0b0973a7>
2010-07-29 16:10:19 +0000

    git-svn-id: https://feedling.svn.sourceforge.net/svnroot/feedling/trunk@45 1b7a0db6-1847-0410-be85-accc0b0973a7

--- Feedling/Classes/FeedConfigItemList.cs
+++ Feedling/Classes/FeedConfigItemList.cs
@@ -1,4 +1,24 @@
-﻿using System.Collections;
+﻿/*
+ * Copyright © 2008-2010, Andrew Rowson
+ * http://www.growse.com
+ * 
+ *  This file is part of Feedling.
+
+    Feedling is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Feedling is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Feedling.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+using System.Collections;
 using System.Xml;
 using System.Xml.Serialization;
 
